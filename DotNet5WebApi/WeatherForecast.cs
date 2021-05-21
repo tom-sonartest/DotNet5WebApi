3fc6462a50d50890c91e31bd5256d987893d7a80
Tom Howlett (Sonarsource) <tom.howlett@sonarsource.com>
2021-05-21 15:17:56 +0100

    Added some dodgy floats

--- DotNet5WebApi/WeatherForecast.cs
+++ DotNet5WebApi/WeatherForecast.cs
@@ -6,9 +6,14 @@ namespace DotNet5WebApi
     {
         public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
+        public float TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public float TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public bool IsFreezing()
+        {
+            return TemperatureC < 0;
+        }
 
         public string Summary { get; set; }
     }
