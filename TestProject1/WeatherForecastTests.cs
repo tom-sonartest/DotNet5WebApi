816f55d68b8d32c67f4c66cf300b2bfe76248b74
Tom Howlett (Sonarsource) <tom.howlett@sonarsource.com>
2021-06-16 15:52:50 +0100

    Added a test to test coverage

---
+++ TestProject1/WeatherForecastTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DotNet5WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet5WebApi.Tests
+{
+    [TestClass()]
+    public class WeatherForecastTests
+    {
+        [TestMethod()]
+        public void IsFreezingTest()
+        {
+            var forecast = new WeatherForecast();
+            forecast.TemperatureC = -6;
+            Assert.IsTrue(forecast.IsFreezing());
+        }
+    }
+}
